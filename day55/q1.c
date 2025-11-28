0e5caac441d762b6a837804f059919dff35b2b34
Sanchigoyal26 <sanchigoyal282@gmail.com>
2025-11-28 20:22:34 +0530

    q1.c

---
+++ day55/q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int count;
+    int majority = -1;
+
+    for (i = 0; i < n; i++) {
+        count = 0;
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == nums[i]) {
+                count++;
+            }
+        }
+        if (count > n / 2) {       // majority condition
+            majority = nums[i];
+            break;
+        }
+    }
+
+    printf("%d\n", majority);
+    return 0;
+}
